542c0d1c530f0c16f878ee0b3c45a42dce19918f
llegregam <legregam@insa-toulouse.fr>
2024-01-10 11:22:44 +0100

    Ecriture d'un docstrings contenant les tâches de rkouakou06.

---
+++ isodesign/io.py
@@ -0,0 +1,27 @@
+"""
+Module contenant la classe qui va gérer les entrées et sorties du logiciel.
+Les différents paramètres de chaque run vont aussi être initialisés et stockées dans cette classe.
+
+Les tâches à effectuer:
+    - Création d'une classe pour contenir les méthodes et attributs associés à la gestion de données et de paramètres
+    - Création d'une méthode pour importer un fichier .netw et stocker dans un attribut (ignorer les lignes commençant
+    par un #)
+    - Création d'une méthode pour importer un fichier .tvar et stocker dans un attribut
+    - Création d'une méthode pour importer un fichier .mflux et stocker dans un attribut (ignorer les lignes commençant
+    par un #)
+    - Créer une méthode __repr__ qui montre les différentes données importées (qui ne renvoie pas d'erreur si un des
+    trois fichiers n'a pas été importé)
+    - Créer une méthode qui permet d'initialiser un objet 'molécule' ou 'traceur' (qui dit objet dit classe... ;) )
+    contenant un noms de molécule et un nombre de carbonnes associés à cette molécules. Trouver un moyen de stocker ces
+    objets dans notre classe. Cette classe doit aussi avoir une fonction __len__ pour retourner la longueur de la
+    chaine de carbonnes et une fonction __repr__ pour montrer joliment le contenu de l'objet initialisé.
+
+N'hésite pas à ecrire un petit test au fur et a mesure que tu codes.
+
+"""
+
+
+
+if __name__ == "__main__":
+    # Mettre ici tes tests
+    pass
