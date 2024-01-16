c1f6c62b89747bd355e0b91fe0e078f782621f02
rkouakou06 <rochellekouakou@gmail.com>
2024-01-16 15:57:13 +0100

    changement des docstrings + code

--- isodesign/process/calculation.py
+++ isodesign/process/calculation.py
@@ -16,8 +16,7 @@ class Tracer:
 
     def __len__(self):
         """ 
-        This method permit to count the carbon number by 
-        using the labeling.
+        Count the carbon number by using the labeling.
         """
         self.num_carbon = len(self.labeling)
 
@@ -27,53 +26,70 @@ class Tracer:
 
 def intra_mol_comb(num_mol, step=10, start=0, end=100):
     """
-    Function that output a list of combination for a molecule family.
-    Input : number of molecule in the family, step (10 by default), start (0 by default) 
-    and (100 by default)
+    Building the intramolecular cominations. Default values :
+        * start: 0
+        * end: 100
+
+    :param num_mol: number of molecules into the family 
+    :param step: 
+    
+    :return: list of tuple containing all the intramolecular combinations
     """
-    fractions = [] #list containing list of fractions in function of the number of molecule 
+
+    all_fracs = [] #list containing list of fractions in function of the number of molecule 
     for _ in range(len(num_mol)):
-        fracs=[data for data in range(start,end,step)] #list of fractions 
-        fractions.append(fracs) #adding list of fraction for a molecule in the fraction list 
-    comb = [i for i in list(product(*fractions)) if sum(i) == 100] #list containing all the combination inside a molecule family  
-    return comb
+        fractions=[data for data in range(start,end,step)] #list of fractions 
+        all_fracs.append(fractions) #adding list of fraction for a molecule in the fraction list 
+    intra_combi = [pair for pair in list(product(*all_fracs)) if sum(pair) == 100] #list containing all the combination inside a molecule family  
+    return intra_combi
 
 
-def inter_mol_comb(mol1,mol2, step=10, start=0, end=100):
+def inter_mol_comb(fam_mol1,fam_mol2, step=10, start=0, end=100):
     """ 
-    Function that output a list of combination between 2 or more molecule family.
+    Building the intermolecular combinations. Default values :
+        * start: 0
+        * end: 100 
+    
+    :param fam_mol1: list of molecule names of the first molecule family   
+    :param fam_mol2: list of molecule names of the second molecule family
+    :param step: 
 
-    Input : mol1 (list of all the molecule for the first family), mol2 (list of all 
-    the molecule for the second family), step (10 by default), start (0 by default) 
-    and (100 by default).
+    :return: list containing tuple which contain tuple with all the intermolecular combinations
 
-    Use the function intra_mol_comb for each molecule family to have the list of combination 
-    inside the molecule family. 
+    Using the function intra_mol_comb for each molecule family to have the 
+    list of combination inside the molecule family. 
     """
-    mol_1 = intra_mol_comb(mol1, start=0, end=100, step=10) #intramolecular combination for the first family 
-    mol_2 = intra_mol_comb(mol2, start=0, end=100, step=10) #intramolecular combination for the second family
-    inter_comb = list(product(mol_1,mol_2)) #list containing all the combination
-    return inter_comb
+    
+    combi_family_1 = intra_mol_comb(fam_mol1) #intramolecular combination for the first family 
+    combi_family_2 = intra_mol_comb(fam_mol2) #intramolecular combination for the second family
+    intermol_combi = list(product(combi_family_1,combi_family_2)) #list containing all the combination
+    return intermol_combi
 
-def generate_file(mol1, mol2,labeling1, labeling2):
+def generate_file(fam_mol1, fam_mol2, label_fam1, label_fam2):
     """ 
-    Function generating .linp files in function of all combination between 2 or more molecule family. 
+    Generating .linp files in function of all combination between 2 molecule family. 
 
-    Input : mol1 (list of all the molecule for the first family), mol2 (list of all the molecule for the 
-    second family), labeling1 (list of all labeling for the first molecule family), labeling2 (list of all 
-    labeling for the second molecule family)
+    :param fam_mol1: list of molecule names of the 1st molecule family   
+    :param fam_mol2: list of molecule names of the 2nd molecule family
+    :param label_fam1: list of all labeling for the 1st molecule family
+    :param label_fam2: list of all labeling for the 2nd molecule family
+
+    :return: generate .linp files depending on the number of combination 
+            between molecules family. Files containing dataframe with tracers
+            features including all the combinations on the column "Value"
     """
+
     df = pd.DataFrame({'Id':None,
                             'Comment':None,
-                            'Specie': [i for i in mol1+mol2],
-                            'Isotopomer' : [j for j in labeling1+labeling2],
+                            'Specie': [i for i in fam_mol1+fam_mol2],
+                            'Isotopomer' : [j for j in label_fam1+label_fam2],
                             'Value':None}) 
     
-    combi = inter_mol_comb(mol1, mol2) #list of all the combination
-    for i in combi:
+    all_combi = inter_mol_comb(fam_mol1, fam_mol2) #list of all the combination
+    for pair in all_combi:
         tmp_df = df.copy() #creation of a temporary file
-        tmp_df["Value"] = i[0] + i[1] #column "Value" take all the combinations values
-        tmp_df.to_csv(f"{i}.csv", sep="\t") #generation the .linp files
+        tmp_df["Value"] = pair[0] + pair[1] #column "Value" take all the combinations values
+        tmp_df.to_csv(f"{pair}.linp", sep="\t") #generation the .linp files
 
 if __name__ == "__main__":
     # Get for Glucose
@@ -89,7 +105,7 @@ if __name__ == "__main__":
     ace_name = [ace_U.name, ace_1.name]
     glu_labeling = [gluc_u.labeling, gluc_1.labeling] #only tracers labeling
     ace_labeling= [ace_U.labeling, ace_1.labeling]
-    print(generate_file(glu_name, ace_name, glu_labeling, ace_labeling))
+    generate_file(glu_name, ace_name, glu_labeling, ace_labeling)
     # # print(mol_1)
     # Get for Acetate
     # mol_2 = comb_fraction(2,0,100,10)
