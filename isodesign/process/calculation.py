7fb55bf1512bfbbf7bd8352723fec414d08d6c65
rkouakou06 <rochellekouakou@gmail.com>
2024-01-18 15:52:49 +0100

    ajout fonction generate_files

--- isodesign/process/calculation.py
+++ isodesign/process/calculation.py
@@ -24,85 +24,105 @@ class Tracer:
         return f"Molecule name: {self.name}, Number of associated carbon(s) : {self.num_carbon}"
 
 
-def generate_frac_comb(molecules, step=10, start=0, end=100):
+def _generate_frac_comb(molecules, step=10):
     """
     Generate combinations of different molecular tracers proportions
 
     :param molecules: list of tracer molecules
     :param step: step for proportions to test
-    :param start: lowest proportion value for tracer molecules in mix
-    :param end: highest proportion value for tracer molecules in mix
     
     :return: list of tuples containing proportions of each tracer in mix (sum must be equant to end param value)
     """
 
     all_fracs = []
     for _ in range(len(molecules)):
-        fractions = [data for data in range(start, end, step)]
+        fractions = [data for data in range(0, 100+step, step)]
         all_fracs.append(fractions)
-
+            
     # list containing all the combination inside a molecule family
     mixes = [frac_list for frac_list in list(product(*all_fracs)) if sum(frac_list) == 100]
     return mixes
-
-
+    
 def generate_mixes_comb(*args):
     """ 
     Generate cominations between two molecular mixes of tracers
     
-    :param args: mixes to combine together
+    :param args:list of mixes to combine together
 
     :return: list of all combinations of the tracer mixes
-    """
-    return list(product(*[generate_frac_comb(mix) for mix in args]))
+    """       
+    if len(args) == 1:
+        return _generate_frac_comb(args[0])
+    return list(product(*[_generate_frac_comb(mix) for mix in args]))
 
 
-def generate_file(fam_mol1, fam_mol2, label_fam1, label_fam2):
+def generate_file(tracer1, tracer2=None):
     """ 
-    Generating .linp files in function of all combination between 2 molecule family. 
+    Generating .linp files in function of all combination for one or two mixe(s)
 
-    :param fam_mol1: list of molecule names of the 1st molecule family   
-    :param fam_mol2: list of molecule names of the 2nd molecule family
-    :param label_fam1: list of all labeling for the 1st molecule family
-    :param label_fam2: list of all labeling for the 2nd molecule family
+    :param tracer1: list of tracer molecules
+    :param tracer2: list of tracer molecules
 
-    :return: generate .linp files depending on the number of combination 
-            between molecules family. Files containing dataframe with tracers
-            features including all the combinations on the column "Value"
+    :return: Generate .linp files depending on the number of combination
+            Files containing dataframe with tracers features including the combinations
+            for all tracer mixes
     """
-
     df = pd.DataFrame({'Id': None,
                        'Comment': None,
-                       'Specie': [i for i in fam_mol1 + fam_mol2],
-                       'Isotopomer': [j for j in label_fam1 + label_fam2],
-                       'Value': None})
+                       'Specie': [],
+                       'Isotopomer': [],
+                       'Value': []} )
+   
+    tmp_df = df.copy()
 
-    all_combi = generate_mixes_comb(fam_mol1, fam_mol2)  # list of all the combination
+    labels = [] #list containing all the labeling 
+    names = [] #list containing all the tracer names
+
+    for trac1 in tracer1:
+        labels.append(trac1.labeling)
+        names.append(trac1.name)
+    
+    all_combi = generate_mixes_comb(tracer1) #list of tuple with all combinations for one mix
+
+    if tracer2:
+        for trac2 in tracer2:
+            labels.append(trac2.labeling)
+            names.append(trac2.name)
+        all_combi = generate_mixes_comb(tracer1, tracer2) #list of tuple of tuple with all combinations for two mixes
+        
+    tmp_df["Isotopomer"] = [labeling for labeling in labels]
+    tmp_df["Specie"] = [name for name in names] 
+
     for pair in all_combi:
-        tmp_df = df.copy()  # creation of a temporary file
-        tmp_df["Value"] = pair[0] + pair[1]  # column "Value" take all the combinations values
-        tmp_df.to_csv(f"{pair}.linp", sep="\t")  # generation the .linp files
-
-
+        if tracer2 :
+            tmp_df["Value"] = pair[0]+pair[1]
+            tmp_df2=tmp_df.drop(tmp_df[tmp_df.Value == 0].index) #remove all row equals to 0 
+            tmp_df2.to_csv(f"{pair}.linp", sep="\t")
+        if not tracer2:
+            tmp_df["Value"] = pair
+            tmp_df2=tmp_df.drop(tmp_df[tmp_df.Value == 0].index)
+            tmp_df2.to_csv(f"{pair}.linp", sep="\t")
+    
+    
 if __name__ == "__main__":
     # Get for Glucose
-    final = generate_mixes_comb(["Ace_U", "Ace_1"], ["Gluc_U", "Gluc_1", "Gluc_23"])
-    print(final)
+    # final = generate_mixes_comb(["Ace_U", "Ace_1"], ["Gluc_U", "Gluc_1", "Gluc_23"])
+    # print(final)
     # print(mol_1)
     # print(mol_2)
     # #print(mol_1)
     # #mol = inter_mol_comb(["Gluc_U", "Gluc_1", "Gluc_23"], ["Ace_U", "Ace_1"], 10, 0,100)
     # #print(mol)
-    # gluc_u = Tracer("Gluc_U", 111111)
-    # gluc_1 = Tracer("Gluc_1", 100000)
+    gluc_u = Tracer("Gluc_U", "111111")
+    gluc_1 = Tracer("Gluc_1", "100000")
+    gluc_23 = Tracer("Gluc_23", "011000")
+    ace_u = Tracer("Ace_U", "11")
+    ace_1 = Tracer("Ace_1", "10")
+    #print(generate_mixes_comb([gluc_u.name,gluc_1.name, gluc_23.name], [ace_u.name, ace_1.name]))
+    #print(generate_mixes_comb([gluc_u.name, gluc_1.name], [ace_u.name, ace_1.name]))
     # ace_U = Tracer("Ace_U", 11)
     # ace_1 = Tracer("Ace_1", 10)
-    # glu_name = [gluc_u.name, gluc_1.name] #only tracers names
-    # ace_name = [ace_U.name, ace_1.name]
-    # glu_labeling = [gluc_u.labeling, gluc_1.labeling] #only tracers labeling
-    # ace_labeling= [ace_U.labeling, ace_1.labeling]
-    # generate_file(glu_name, ace_name, glu_labeling, ace_labeling)
-    # # # print(mol_1)
+    print(generate_file([gluc_u, gluc_1],[ace_u,ace_1]))
     # Get for Acetate
     # mol_2 = comb_fraction(2,0,100,10)
     # print(mol_2)
