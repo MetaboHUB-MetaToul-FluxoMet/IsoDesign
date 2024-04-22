1e118c1fa6d8d4213fd9b268ab87b66967cc54d6
rkouakou6 <rochellekouakou@gmail.com>
2024-04-22 14:29:05 +0200

    added continuation of "substrates selection" form

--- isodesign/ui/pages/2_Substrates_selection.py
+++ isodesign/ui/pages/2_Substrates_selection.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from sess_i.base.main import SessI
-from collections import namedtuple
-from isodesign.base.calculation import Process, Isotopomer, LabelInput
+from isodesign.base.calculation import Isotopomer
 
 
 session = SessI(
@@ -22,6 +21,7 @@ def add_substrates(substrate_name, labelling, nb_intervals, lower_b, upper_b):
     # Create an Isotopomer object with the given parameters
     iso = Isotopomer(substrate_name, labelling, int(nb_intervals), int(lower_b), int(upper_b))
     if f"{substrate_name}" not in session.object_space.objects:
+        # Register the Isotopomer object in the session object space with a unique key
         session.register_object([iso], key=f"{substrate_name}")
     else:
         session.object_space.objects[f"{substrate_name}"].append(iso)
@@ -47,7 +47,7 @@ substrates = session.object_space["process_object"].netan["input"]
 # Create columns based on number of inputs
 ncols = st.columns(len(substrates), gap="medium")
 for index, substrate_name in enumerate(substrates):
-    # Column creation
+    # Column creation depending on the number of substrates
     with ncols[index]:
         # Column title with input name
         st.header(substrate_name)
@@ -70,10 +70,29 @@ for index, substrate_name in enumerate(substrates):
                                 on_click=add_substrates, 
                                 args=(substrate_name, labelling, nb_intervals, lower_b, upper_b))
         
-        
+st.write(" ")        
 substrates_selection = st.form("form_substrate")
 with substrates_selection:
-    st.header("Selected substrates")
-    st.write(session)
-    # Create a button to launch the calculation
+    st.header("Labels")
+    # Create a copy of the dictionary without the "process_object" key
+    # Session.object_space contains all objects registered in previous pages
+    # Here, the "process_object" object created in the previous page must not be used for the methods to follow 
+    objects_without_process = {key: value for key, value in session.object_space.objects.items() if key != "process_object"}
+
+    # display of initiated labels 
+    for substrates_name, labelinputs in objects_without_process.items():
+        st.header(substrates_name)
+        for labelinput in labelinputs:
+            st.write(labelinput)
+    
     submit_button = st.form_submit_button("Submit")
+
+    if submit_button:
+        # generating combinations 
+        session.object_space["process_object"].generate_combinations(objects_without_process)
+        # linp file generations 
+        session.object_space["process_object"].generate_linp_files()
+        session.object_space["process_object"].generate_vmtf_file()
+        # session.object_space["process_object"].files_copy()
+        
+        st.switch_page(r"pages/3_Influx_simulation.py")
