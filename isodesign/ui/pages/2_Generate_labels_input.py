ef0c830131a42cc3a9f782819d492444ac0cd090
rkouakou06 <rochellekouakou@gmail.com>
2024-06-25 16:36:05 +0200

    changement méthode remove_substrates

--- isodesign/ui/pages/2_Generate_labels_input.py
+++ isodesign/ui/pages/2_Generate_labels_input.py
@@ -1,45 +1,40 @@
 import streamlit as st
 from sess_i.base.main import SessI
-from isodesign.base.isotopomer import Isotopomer
 
 
 session = SessI(
         session_state=st.session_state,
         page="2_Generate_labels_input2.py")
 
-def add_substrates(substrate_name, labelling, nb_intervals, lower_b, upper_b):
+# Retrieving substrates names from sessI
+process_object = session.object_space["process_object"]
+
+
+def remove_substrates(substrate, labelling):
     """
-    Create an Isotopomer object from the substrate add
+    Remove substrates from the isotopomer dictionary.
+    In the dictionary, all substrates have the same name. They can 
+    therefore be differentiated by labelling. This parameter is used 
+    to remove the correct substrate. 
+    
+    :param substrate: substrate name
+    :param labelling: labelling for isotopomer"""
+    
+    for substrate in process_object.isotopomer_dict[f"{substrate_name}"]:
+        if substrate.labelling == labelling:
+            process_object.isotopomer_dict[f"{substrate_name}"].remove(substrate)
 
-    :param substrate_name: substrate name  
-    :param labelling: labelling 
-    :param nb_intervals: number of intervals (step)
-    :param lower_b: lower bound
-    :param upper_b: upper bound
-    """
 
-    # Create an Isotopomer object with the given parameters
-    iso = Isotopomer(substrate_name, labelling, int(nb_intervals), int(lower_b), int(upper_b))
-    if f"{substrate_name}" not in session.object_space.objects:
-        # Register the Isotopomer object in the session object space with a unique key
-        session.register_object([iso], key=f"{substrate_name}")
-    else:
-        session.object_space.objects[f"{substrate_name}"].append(iso)
-
-def remove_substrates(substrate_name, labelling):
-    if f"{substrate_name}" in session.object_space.objects:
-        for value in session.object_space.objects[f"{substrate_name}"]:
-            if value.labelling == labelling:
-                session.object_space.objects[f"{substrate_name}"].remove(value)
-        # session.object_space.objects[f"s{substrate_name}"].remove(iso)
+       
 
 def get_carbon_length(substrate):
     """
-    Obtain carbon length from the substrate
+    Obtain the carbon number of the substrate. 
+    This number is retrieved from the netan dictionary (Process class). 
 
     :param substrate: substrate name  
     """
-    carbon_length = session.object_space["process_object"].netan["Clen"]
+    carbon_length = process_object.netan["Clen"]
     if substrate in carbon_length:
         return carbon_length[substrate]
 
@@ -54,7 +49,7 @@ def add_form(count=0):
     
     with st.form(f"form_{substrate_name}_{count}"):
         labelling = st.text_input(f"Number of tracer atoms : {get_carbon_length(substrate_name)}", 
-                                  key=f"text_{substrate_name}_{count}", 
+                                  key=f"labelling_{substrate_name}_{count}", 
                                   value="0" * get_carbon_length(substrate_name))
         
 
@@ -65,52 +60,51 @@ def add_form(count=0):
             upper_b = st.text_input("Upper bound", key=f"ub_{substrate_name}_{count}", value=100)
         with step:
             nb_intervals = st.text_input("Nb intervals", key=f"step_{substrate_name}_{count}", value=100)
-
+        # 
         add_col, remove_col = st.columns([1, 10])
         with add_col:
             add = st.form_submit_button("Add")
             if add:
                 session.register_widgets({f"add_{count}_{substrate_name}" : add})
-                add_substrates(substrate_name, labelling, nb_intervals, lower_b, upper_b)
-                st.rerun()
+                process_object.generate_isotopomer(substrate_name, labelling, int(nb_intervals), int(lower_b), int(upper_b))
         if count > 0:
             with remove_col:
                 remove = st.form_submit_button("Remove")
                 if remove:
                     remove_substrates(substrate_name, labelling)
-                    session.widget_space.widgets.pop(f"add_{count}_{substrate_name}")
-                    st.rerun()
+                #     session.widget_space.widgets.pop(f"add_{count}_{substrate_name}")                  
 
     if session.widget_space[f"add_{count}_{substrate_name}"]:
         count += 1
+        # The function calls itself to generate a new form to add a new substrate 
         add_form(count)
         
 
 st.set_page_config(page_title="IsoDesign 2.0")
 st.title("Generate labels input")
 
+# Add a space between page title and content 
 st.write(" ")
-# Retrieving substrates names from sessI
-substrates = session.object_space["process_object"].netan["input"]
-# Create tabs based on number of inputs
-tabs=st.tabs(list(substrates))
-for index, substrate_name in enumerate(substrates):
+# Create tabs according to the number of labeled substrates 
+# Labeled substrates are retrieved from the netan dictionary (argument in Process class) 
+tabs=st.tabs(list(process_object.netan["input"]))
+for index, substrate_name in enumerate(process_object.netan["input"]):
     with tabs[index]:
         st.header(substrate_name)
+        # Add a form to add labeled substrates
         add_form()
 
 
 submit = st.button("Submit")
 if submit:
-    objects_without_process = {key: value for key, value in session.object_space.objects.items() if key != "process_object"}
-    session.object_space["process_object"].generate_combinations(objects_without_process)
+    session.object_space["process_object"].generate_combinations()
     # linp file generations 
     session.object_space["process_object"].generate_linp_files()
     session.object_space["process_object"].generate_vmtf_file()
     session.object_space["process_object"].copy_files()
     st.switch_page(r"pages\3_Simulation_options.py")
 
-# st.write(session)
+st.write(session.object_space["process_object"].isotopomer_dict)
 
 
     
