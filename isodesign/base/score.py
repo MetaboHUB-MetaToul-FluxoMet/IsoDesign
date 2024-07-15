e03472f757c8e3b9eb93a95c87be51358c17fc0c
rkouakou06 <rochellekouakou@gmail.com>
2024-07-15 13:45:58 +0200

    modification of methods in ScoreHandler class

--- isodesign/base/score.py
+++ isodesign/base/score.py
@@ -7,30 +7,34 @@ logger = logging.getLogger(f"IsoDesign.{__name__}")
 
 class Score:
     """
-    A class to compute the score of a series of values
+    Class containing the implementation of the rating criteria 
     """
-    def __init__(self, series):
-        # The different scoring methods that can be applied. Key: method name, value: method to be applied 
+    def __init__(self, sd_fluxes_columns):
+        """
+        :param sd_fluxes_columns: the SDs of the fluxes for a given label input are contained as columns in the summary dataframe
+        """
+
+        # The different rating methods that can be applied. Key: method name, value: method to be applied 
         self.SCORING_METHODS = {
         "sum_sd" : self.apply_sum_sd, 
         "number_of_flux" : self.apply_sum_nb_flux_sd,  
         "labeled_input" : self.apply_number_labeled_inputs,
         }
 
-        # Fluxes SDs according to the different label input's proportions ar contained as columns in the summary dataframe
-        # The series of values to compute the score
-        self.series = series
-        # The calculated score of the series
+        self.sd_fluxes_columns = sd_fluxes_columns 
         self.score = None
 
     def __call__(self):
+        """
+        Returns the score obtained after applying the criteria
+        """
         return self.score
     
     def compute(self, method, **kwargs):
         """
         Computes the score for a given method
         
-        :param method: the method to apply to the series
+        :param method: the method to apply to the sd_fluxes_columns
         :param kwargs: additional arguments to pass to the method
         
         """
@@ -41,7 +45,7 @@ class Score:
                 self.score = self.SCORING_METHODS[method](kwargs["labeled_species_dict"], kwargs["weight_labeled_input"] if "weight_labeled_input" in kwargs else 1)
             case "sum_sd":
                 self.score = self.SCORING_METHODS[method](kwargs["weight_sum_sd"] if "weight_sum_sd" in kwargs else 1)
-        logger.info(self.score)
+       
 
     def apply_sum_sd(self, weight_sum_sd=1):
         """
@@ -49,7 +53,7 @@ class Score:
 
         :param weight: he weight to apply to the score
         """
-        return self.series.sum() * weight_sum_sd
+        return self.sd_fluxes_columns.sum() * weight_sum_sd
         
     
     def apply_sum_nb_flux_sd(self, threshold, weight_flux=1):
@@ -60,7 +64,7 @@ class Score:
         :param weight: he weight to apply to the score
         """
 
-        return (self.series < threshold).sum() * weight_flux
+        return (self.sd_fluxes_columns < threshold).sum() * weight_flux
     
     def apply_number_labeled_inputs(self, labeled_species_dict, weight_labeled_input=1):
         """
@@ -68,8 +72,8 @@ class Score:
 
         :param labeled_species_dict: a dictionary containing the labelled species
         """
-        if self.series.name in labeled_species_dict.keys():
-            return labeled_species_dict[self.series.name] * weight_labeled_input
+        if self.sd_fluxes_columns.name in labeled_species_dict.keys():
+            return labeled_species_dict[self.sd_fluxes_columns.name] * weight_labeled_input
 
 
     # def identified_structures(self, tvar_sim_paths_list):
@@ -86,64 +90,57 @@ class Score:
     #         struc_identified.update({f"{file_name}_SD" : sum([len(struct) for struct in pd.read_csv(file, sep="\t", usecols=["Struct_identif"]).values if struct == 'yes'])}) 
     #     return struc_identified
     #     # return pd.DataFrame({"Identified structures": struc_identified})
-    
+        
 
 class ScoreHandler:
     """ 
-    This class applies score methods, performs operations between them 
-    and displays them on a given dataset. 
+    This class applies rating methods and performs operations between them.
+    . 
     """
     def __init__(self, dataframe):
-        # the summary dataframe (filtered or not) of the results of the various label input proportions following the simulation
+        """
+        :param dataframe: the summary dataframe (filtered or not) of the results of the various label input proportions following the simulation
+        
+        """
+        
         self.dataframe = dataframe
-        # dictionary containing the results of scoring methods applied to the columns (fluxes SDs based on different proportions of label inputs) of the dataframe
-        # Key : column name, value : dictionary containing the score method as key and the score as value
+        # dictionary containing the results of rating methods applied to the dataframe columns
+        # Key : column name, value : dictionary containing the rating method as key and the score as value
         self.columns_scores = {}
     
 
-    def apply_scores(self, score_method : list, **kwargs):
+    def apply_criteria(self, criteria : list, **kwargs):
         """ 
-        Apply a score method to the dataframe columns via the Score class
+        Apply criteria to the dataframe columns via the score_oject class
         
-        :param score_method: the method to apply to the dataframe columns
-        :param kwargs: additional arguments to pass to the score method
+        :param criteria: method(s) to apply to the columns
+        :param kwargs: additional arguments 
         """
         for column in self.dataframe.columns:
-            # Each database column is converted into a Score object to offer flexibility in the subsequent manipulation of each score
-            score = Score(self.dataframe[column])
-            for method in score_method:
-                score.compute(method, **kwargs)
-                self.columns_scores.update({column: {method: score.score}}) if column not in self.columns_scores.keys() else self.columns_scores[column].update({method: score.score})
+            # Each dataframe column is converted into a score_oject object to offer flexibility in the subsequent manipulation of each score
+            score_oject = Score(self.dataframe[column])
+            for method in criteria:
+                score_oject.compute(method, **kwargs)
+                self.columns_scores.update({column: {method: score_oject()}}) if column not in self.columns_scores.keys() else self.columns_scores[column].update({method: score_oject()})
 
-
-    def get_scores(self, columns_names=None, operation=None):
+    def apply_operations(self, operation):
         """
-        Returns as a dataframe the results of the scoring method(s) 
-        applied according to the desired columns (fluxes SDs based on 
-        different proportions of label inputs). 
-        Operations can be applied between score results 
-
-        :param columns_names: the columns to return the scores for
-        :param operation: the operation to apply to the scores (Addition, Multiply, Divide)
-
+        Apply an operation to the score_ojects of the columns
+        
+        :param operation: the operation to apply to the score_ojects (Addition, Multiply, Divide)
         """
-        if columns_names is None:
-            # If no columns are specified, all columns are considered
-            columns_names = self.columns_scores.keys()
-        match operation:
+        match operation: 
             case "Addition":
-                # The scores of the columns are added together
-                # functools.reduce() function is used to apply a particular function passed in its argument to all of the list elements mentioned in the sequence passed along.
-                return pd.DataFrame.from_dict({col : functools.reduce(lambda x, y: x + y, self.columns_scores[col].values()) for col in columns_names},
-                                              orient='index')                            
+                for score_oject in self.columns_scores.values():
+                    score_oject.update({"Addition" : functools.reduce(lambda x, y: x + y, score_oject.values())})
+                return score_oject
             case "Multiply":
-                # The scores of the columns are multiplied together
-                return pd.DataFrame.from_dict({col : functools.reduce(lambda x, y: x * y, self.columns_scores[col].values()) for col in columns_names},
-                                              orient='index')
+                for score_oject in self.columns_scores.values():
+                    score_oject.update({"Multiply" : functools.reduce(lambda x, y: x * y, score_oject.values())})
+                return score_oject
             case "Divide":
-                # The scores of the columns are divided together
-                return pd.DataFrame.from_dict({col : functools.reduce(lambda x, y: x / y, self.columns_scores[col].values()) for col in columns_names},
-                                              orient='index')
-        # If no operation is specified, the scores are returned as they are
-        return pd.DataFrame.from_dict({col: self.columns_scores[col] for col in columns_names}, 
-                                     orient='index')
+                for score_oject in self.columns_scores.values():
+                    score_oject.update({"Divide" : functools.reduce(lambda x, y: x / y, score_oject.values())})
+                return score_oject
+
+
