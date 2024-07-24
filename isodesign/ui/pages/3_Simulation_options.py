fae594bc73220daaf93a7596fa48feeb02d890b6
rkouakou06 <rochellekouakou@gmail.com>
2024-07-24 14:02:23 +0200

    Update simulation options page

--- isodesign/ui/pages/3_Simulation_options.py
+++ isodesign/ui/pages/3_Simulation_options.py
@@ -5,12 +5,13 @@ session = SessI(
         session_state=st.session_state,
         page="3_Simulation_options.py")
 
-st.set_page_config(page_title="IsoDesign 2.0")
+
+st.set_page_config(page_title="IsoDesign")
 st.title("Simulation options")
 
 st.write(" ")
 
-command_list=["--prefix", session.object_space["process_object"].prefix]
+param_list=[]
 feasability_domain = st.checkbox("Feasibility domain projection",
                                  help="Project the feasibility domain of the model")
 
@@ -21,7 +22,7 @@ emu = st.checkbox("Elementary Metabolite Units (EMU)",
                   help="Use Elementary Metabolite Units (EMU) for the simulation")
 
 if emu == True:
-    command_list.append("--emu")
+    param_list.append("--emu")
 
 
 no_scale = st.checkbox("No scale", 
@@ -29,14 +30,14 @@ no_scale = st.checkbox("No scale",
                        value=True,
                        help="Do not scale the fluxes")
 if no_scale:
-    command_list.append("--noscale")
+    param_list.append("--noscale")
 
 ln = st.checkbox("Least norm solution (ln)", 
                  key="--ln", 
                  value=True,
                  help="Use the least norm solution (ln) for the simulation")
 if ln:
-    command_list.append("--ln")
+    param_list.append("--ln")
 
 noopt = st.checkbox("No optmization (noopt)", 
                     key="--noopt", 
@@ -44,12 +45,12 @@ noopt = st.checkbox("No optmization (noopt)",
                     help="Do not perform optimization")
 
 if noopt:
-    command_list.append("--noopt")
+    param_list.append("--noopt")
 
 add_options = st.text_input("Add options", 
                             key="add_options")
 if add_options:
-    command_list.append(f"--{add_options}")
+    param_list.append(f"--{add_options}")
 
 session.register_widgets({"--emu": emu,
                           "--noscale": no_scale,
@@ -57,13 +58,9 @@ session.register_widgets({"--emu": emu,
                           "add_options": add_options,
                           "feasability_domain": feasability_domain})
 
-session.register_widgets({"emu": emu})
-
-# st.write(session)
-# st.write(command_list)
 
 submit = st.button("Submit")
 if submit:
-    session.object_space["process_object"].influx_simulation(command_list)
+    session.object_space["process_object"].influx_simulation(param_list)
     session.object_space["process_object"].generate_summary()
     st.switch_page(r"pages/4_Analysis.py")
