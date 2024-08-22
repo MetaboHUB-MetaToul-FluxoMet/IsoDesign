0f907f3f37b6429bb9c4fa5306f5f4c4e86352f0
rkouakou06 <rochellekouakou@gmail.com>
2024-08-22 11:52:58 +0200

    add "price" to the criteria

--- isodesign/ui/pages/4_Analysis.py
+++ isodesign/ui/pages/4_Analysis.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from sess_i.base.main import SessI
 import pandas as pd
-from matplotlib import pyplot as plt
+
 
 session = SessI(
         session_state=st.session_state,
@@ -40,35 +40,36 @@ with st.expander("**Apply a filter**"):
     st.write("Filter by : ")
     fluxes_name, kind, pathway = st.columns(3)
     with fluxes_name:
-        selected_flux=(st.multiselect(label="Fluxes_name", 
+        selected_flux=st.multiselect(label="Fluxes_name", 
                                     options=filtered_dataframe["Name"].drop_duplicates() if filtered_dataframe is not None else summary_dataframe["Name"].drop_duplicates(), 
                                     label_visibility="collapsed",
-                                    placeholder= "Flux"))
+                                    placeholder= "Flux")
         
     with kind:
-        selected_kind=(st.multiselect(label="kind", 
+        selected_kind=st.multiselect(label="kind", 
                                     options=["NET", "XCH", "METAB"], 
                                     label_visibility="collapsed", 
-                                    placeholder="Kind"))
-        
+                                    placeholder="Kind")
+    
     with pathway:
-        selected_pathway=(st.multiselect(label="Pathway", 
-                                        options=process_object.netan["pathway"].keys(), 
-                                        label_visibility="collapsed", placeholder="Pathway"))
+        selected_pathway=st.multiselect(label="Pathway", 
+                                        options=process_object.netan["pathway"].keys() if process_object.netan["pathway"] else ["No pathway"], 
+                                        label_visibility="collapsed", 
+                                        placeholder="Pathway",
+                                        disabled=True if not process_object.netan["pathway"] else False)
         
     
     submit=st.button("Submit",
                     on_click=filter,
                     args=(selected_flux, selected_kind, selected_pathway))
 
-
-    
+  
 with st.container(border=True):
     # Container divided into 2 columns, one for selecting and configuring columns and the other for displaying results 
     methods, result = st.columns([6, 9], gap="large")
     with methods:
         method_choice = st.multiselect("Criterion", 
-                                        options=["sum_sd", "number_of_flux", "labeled_input"],
+                                        options=["sum_sd", "number_of_flux", "number_of_labeled_inputs", "price"],
                                         )
         session.register_widgets({"method_choice": method_choice})
 
@@ -85,12 +86,11 @@ with st.container(border=True):
                                                 key=f"threshold", 
                                                 value=1)
             
-        if "labeled_input" in method_choice:
+        if "number_of_labeled_inputs" in method_choice:
             input_labeled_input=st.text_input("Weight_labeled_input", 
                                               key=f"weight_labeled_input", 
                                               value=1)
             
-
         # If more than one method is selected, the user can choose the operation to apply
         if len(method_choice) > 1:
             operation = st.selectbox("Operations", 
@@ -108,8 +108,8 @@ with st.container(border=True):
                                       weight_sum_sd=int(weight_sd) if "sum_sd" in method_choice else 1,
                                         threshold=int(input_threshold) if "number_of_flux" in method_choice else 1,
                                         weight_flux=int(weight_flux) if "number_of_flux" in method_choice else 1,
-                                        labeled_input_dict=dict(process_object.labeled_inputs) if "labeled_input" in method_choice else None,
-                                        weight_labeled_input=int(input_labeled_input) if "labeled_input" in method_choice else 1
+                                        labeled_input_dict=dict(process_object.linp_file_info) if "number_of_labeled_inputs" or "price" in method_choice else None,
+                                        weight_labeled_input=int(input_labeled_input) if "number_of_labeled_inputs" in method_choice else 1
                                       )
         session.register_widgets({"submit_container": submitted})
     
