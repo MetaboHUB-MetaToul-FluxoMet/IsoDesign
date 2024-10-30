0df219603bc13dbc4f55c94578820d56bdfd2221
rkouakou06 <kouakou@insa-toulouse.fr>
2024-10-30 16:09:36 +0100

    Added functionality to add/remove options for influx_si simulation

--- isodesign/ui/pages/3_Simulation_options.py
+++ isodesign/ui/pages/3_Simulation_options.py
@@ -1,7 +1,5 @@
 import streamlit as st
 from sess_i.base.main import SessI
-import streamlit.runtime.scriptrunner as scriptrunner
-import concurrent.futures 
 
 session = SessI(
         session_state=st.session_state,
@@ -17,25 +15,23 @@ st.sidebar.link_button("Documentation",
                   )
 st.write(" ")
 
-# Get the script run context
-script_run_ctx = scriptrunner.get_script_run_ctx(suppress_warning=True)
-
 process_object = session.object_space["process_object"]
 
-def influx_sim():
+def clear_text_input():
+    """ 
+    Clear the "add_options" widget (st.text_input 
+    widget) once the option has been added. 
     """
-    Run the simulations in parallel with the selected mode.
+    # Save the option in the SessI before clearing the widget from the session state
+    session.register_widgets({"add_options": st.session_state["add_options_text_input"]}) 
+    st.session_state["add_options_text_input"] = ""
 
-    This function utilizes a ThreadPoolExecutor to run the influx_simulation
-    method of the process_object in parallel. It also adds the script run 
-    context to each thread in the executor.
+def delete_option(option):
     """
-    
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(process_object.influx_simulation, mode)
-        for processes in executor._threads:
-            scriptrunner.add_script_run_ctx(processes, script_run_ctx)
-
+    Delete an option from the list of added options.
+    """
+    session.widget_space["list_added_options"].remove(option)
+    process_object.command_list.remove(f"--{option}")    
 
 if not process_object :
     st.warning("Please load a metabolic network model in 'Upload data' page.")
@@ -45,8 +41,9 @@ elif not process_object.isotopomers:
 else:
     # Command to be passed to the simulation
     # The command is initialized with the prefix and default options
-    command :list = ["--prefix", process_object.model_name, "--noopt"]   
+    process_object.command_list = ["--prefix", process_object.model_name, "--noopt"]   
 
+    # Select the influx mode
     mode = st.selectbox("Influx mode", 
                         options=["influx_s", "influx_i"],
                         index=0,
@@ -57,49 +54,84 @@ else:
 
 
     with st.container(border=True):
+        # Emu option
         emu = st.checkbox("Elementary Metabolite Units (EMU)", 
                         key="emu", 
                         value=True,
                         help="Use Elementary Metabolite Units (EMU) for the simulation")
-
+        session.register_widgets({"emu": emu})
+        
         if emu == True:
-            command.append("--emu")
-
+            process_object.command_list.append("--emu")
+        
+        # No scale option only if influx_i mode
         if mode == "influx_i":
             no_scale = st.checkbox("No scale", 
                                     key="--noscale", 
                                     value=True,
                                     help="Do not scale the fluxes")
+            session.register_widgets({"no_scale": no_scale})
+            
             if no_scale:
-                command.append("--noscale")
+                process_object.command_list.append("--noscale")
 
+        # Least norm solution option
         ln = st.checkbox("Least norm solution (ln)", 
                         key="--ln", 
                         value=True,
                         help="Use the least norm solution (ln) for the simulation")
+        
+        session.register_widgets({"ln": ln})
+        
         if ln:
-            command.append("--ln")
+            process_object.command_list.append("--ln")
 
+        # Add options manually 
         add_options = st.text_input("Add options", 
-                                    key="add_options")
-        if add_options:
-            command.append(f"--{add_options}")
-   
-    st.info(f"{len(process_object.labelled_substrates_combs)} combinations will be simulated.")
-    st.info(f"Command to run: {command}")
+                                    key="add_options_text_input")
+
+        add = st.button("Add",
+                        key="add_button",
+                        on_click=clear_text_input)
+        
+        session.register_widgets({"add": add})
+
+        if session.widget_space["add"]:
+            # Create a list (if there isn't one) to store the options added to the SessI widgets
+            if not session.widget_space["list_added_options"]:
+                session.register_widgets({"list_added_options": [session.widget_space["add_options"]]})
+            # Check if the option is already added
+            elif session.widget_space["add_options"] in session.widget_space["list_added_options"]:
+                st.warning("Option already added.")
+            else:
+                session.widget_space["list_added_options"].append(session.widget_space["add_options"])
+            
+        if session.widget_space["list_added_options"]:
+            st.subheader("Added options")
+            for option in session.widget_space["list_added_options"]:
+                # Add the option to the command list
+                process_object.command_list.append(f"--{option}")
+                show_options, deletion = st.columns([0.05, 0.1])
+                with show_options:
+                    st.info(option)
+                with deletion:
+                    delete = st.button(label=":x:", 
+                                            key=f"delete_{option}",
+                                            on_click=delete_option,
+                                            args=(option,))
+
+    st.info(f"{len(process_object.linp_dataframes)} combinations will be simulated.")
+    st.info(f"Command to run: {process_object.command_list}")
     
     submit = st.button("Submit")
+
     if submit:
-        process_object.command_list = command
-        try:
-            with st.spinner("Simulation in progress..."):
-                influx_sim()
-                process_object.generate_summary()
+        with st.spinner("Simulation in progress..."):
+            process_object.influx_simulation(mode)
+            process_object.generate_summary()
             st.success("Simulation completed ! ")
-            st.switch_page(r"pages/4_Results_Analysis.py")
-        except Exception as e:
-            st.error(f"An error occured during the simulation : {e}")
-            
+        st.switch_page(r"pages/4_Results_Analysis.py")
+        
         
         
         
